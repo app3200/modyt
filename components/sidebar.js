005a7492f05235771e8d92e36e35cb73641a40f3
app3200 <appattanaik3200@gmail.com>
2021-12-26 14:26:32 +0530

    first

---
+++ components/sidebar.js
@@ -0,0 +1,26 @@
+function sidebar(){
+    return `<div id=sidebar>
+    <div class="li2" style="margin-top: 25px;">
+        <i class="fas fa-list"></i>
+    </div>
+    <div class="li2" >
+        <i class="fas fa-home"></i>
+        <p style="font-size: 16px;line-height:0px;">Home</p>
+    </div>
+    <div class="li2" >
+        <i class="far fa-compass"></i>
+        <p style="font-size: 16px;line-height:0px;">Explore</p>
+    </div>
+    <div class="li2" >
+        <i class="fab fa-youtube"></i>
+        <p style="font-size: 16px;line-height:0px;">Subscription</p>
+    </div>
+    <div class="li2" >
+        <i class="fas fa-file-video"></i>
+        <p style="font-size: 16px;line-height:0px;">Library</p>
+    </div>
+
+</div>`
+}
+
+export default sidebar
